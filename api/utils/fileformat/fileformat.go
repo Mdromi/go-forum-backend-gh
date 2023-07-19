4becf0b914117f46a5dc0fd4b1fd1caa9378bc8d
mdromi <mdromi14@gmail.com>
2023-07-19 08:38:04 -0400

    error handaling

---
+++ api/utils/fileformat/fileformat.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"path"
+	"strings"
+
+	"github.com/twinj/uuid"
+)
+
+func UniqueFormat(fn string) string {
+	// path.Ext() get the extension of the file
+	fileName := strings.TrimSuffix(fn, path.Ext(fn))
+	extension := path.Ext(fn)
+	u := uuid.NewV4()
+	newFileName := fileName + "_" + u.String() + extension
+
+	return newFileName
+}
